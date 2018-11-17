f2e62e4774caa0436a2cc701b31c4654bb93bf66
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 00:37:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(arr)
+  new_arr = []
+  if block_given?
+    i = 0
+    while i < arr.length
+      element = yield(arr[i])
+      if element == ""
+        new_arr << arr[i]
+      else
+        new_arr << yield(arr[i])
+      end
+      i+=1
+    end
+  end
+  return new_arr
+end
--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -1,3 +1,6 @@
+require_relative './spec_helper.rb'
+require_relative '../lib/my_collect.rb'
+
 describe "my_collect" do
   let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
   let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
